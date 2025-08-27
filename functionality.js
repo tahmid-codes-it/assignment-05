44dd2dc1e42d960bb1eafce7dce86d83dd74a835
tahmid-codes-it <mdtahmidiiuc@gmail.com>
2025-08-28 00:47:24 +0600

    completed the alert section, coin section with functionality

--- functionality.js
+++ functionality.js
@@ -23,33 +23,67 @@
 
 //     let heartCount = 0;
 //     favCountNumberTotal = favCountNumber + 1;
-     
+
 //     const favCountNumberElement = favCountNumber.innerText;
 //     favCountNumberElement = favCountNumberTotal;
 //     return favCountNumberElement;
 // })
 
 
-function heartHandler( id){
-     document.getElementById(id).addEventListener("click", function() {
+function heartHandler(id) {
+  document.getElementById(id).addEventListener("click", function () {
 
     const favCount = document.getElementById("fav-count");
-    const favCountNumber = parseInt(favCount.innerText); // current number
+    const favCountNumber = parseInt(favCount.innerText);
 
-    const favCountNumberTotal = favCountNumber + 1; // add 1
-    favCount.innerText = favCountNumberTotal; // update DOM
-})};
+    const favCountNumberTotal = favCountNumber + 1;
+    favCount.innerText = favCountNumberTotal;
+  })
+};
 
 
 
-heartHandler ("heart-1");
-heartHandler ("heart-2");
-heartHandler ("heart-3");
-heartHandler ("heart-4");
-heartHandler ("heart-5");
-heartHandler ("heart-6");
-heartHandler ("heart-7");
-heartHandler ("heart-8");
-heartHandler ("heart-9");
+heartHandler("heart-1");
+heartHandler("heart-2");
+heartHandler("heart-3");
+heartHandler("heart-4");
+heartHandler("heart-5");
+heartHandler("heart-6");
+heartHandler("heart-7");
+heartHandler("heart-8");
+heartHandler("heart-9");
 
-   
+
+
+
+function callHandler(id, message) {
+  document.getElementById(id).addEventListener("click", function () {
+    const coinCount = document.getElementById("coin-count");
+    const coinCountNumber = parseInt(coinCount.innerText);
+    const coinCountNumberTotal = coinCountNumber - 20;
+    if (coinCountNumberTotal === -20) {
+      alert("Not enough coins!");
+      return;
+    }
+ 
+    coinCount.innerText = coinCountNumberTotal;
+    if (coinCountNumberTotal >= 0) {
+      alert(message);
+    }
+
+
+
+  })
+}
+
+
+
+callHandler("call-1",  "📞 Calling National Emergency Number 999...");
+callHandler("call-2", "📞 Calling Police Helpline Number 999...");
+callHandler("call-3", "📞 Calling Fire Service Number 999...");
+callHandler("call-4", "📞 Calling Ambulance Service 1994-999999...");
+callHandler("call-5", "📞 Calling Women & Child Helpline 109...");
+callHandler("call-6", "📞 Calling Anti-Corruption Helpline 106...");
+callHandler("call-7", "📞 Calling Electricity Helpline 16216...");
+callHandler("call-8", "📞 Calling Brac Helpline 16445...");
+callHandler("call-9", "📞 Calling Bangladesh Railway Helpline 163...");
